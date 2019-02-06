f732f3c6e49ec539bb848734fcc02784b1b41fec
klitscher <672@holbertonschool.com>
2019-02-06 22:51:52 +0000

    Adding exercise for printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Printing to stdout using printf
+ *
+ * Return: 0 for exit
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
