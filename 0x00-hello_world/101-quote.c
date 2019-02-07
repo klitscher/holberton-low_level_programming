43fe8d49c1884ab752c8bb7270354a17eabf80cb
klitscher <672@holbertonschool.com>
2019-02-07 04:31:07 +0000

    Adding double quote back in

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-	char sl[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+	char sl[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, sl, strlen(sl));
 	return (1);
