75a4fc8efcd1611c70c8de046fdef98cbaf1af68
klitscher <672@holbertonschool.com>
2019-02-07 22:27:43 +0000

    Adding file for exercise alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Function to print the alphabet
+ *
+ * Return: - should return 0
+ */
+int main(void)
+{
+	char a;
+	a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
