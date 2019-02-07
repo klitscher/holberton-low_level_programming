9aca2de3269f474129154a81966bc86ae5740be3
klitscher <672@holbertonschool.com>
2019-02-07 19:19:21 +0000

    Adding exercise for modulo use and thinking a bit outside the box

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - function to print the last digit
+ *
+ * Return: should return 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, (n % 10));
+	if (n == 0)
+		printf("Last digit of %i is %i and is 0\n", n, (n % 10));
+	if (n < 6 && n != 0)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, (n % 10));
+	return (0);
+}
