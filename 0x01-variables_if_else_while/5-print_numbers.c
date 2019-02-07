989d76b1a9ce538011e376209e173e7ebff9287d
klitscher <672@holbertonschool.com>
2019-02-07 23:15:02 +0000

    Adding exercise for printing single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Function for printing all the singles
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n <= 9)
+	{
+		printf("%i", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
