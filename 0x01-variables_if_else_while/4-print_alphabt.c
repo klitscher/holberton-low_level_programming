e09fc952c78d48180111370a374b43abba6df2bf
klitscher <672@holbertonschool.com>
2019-02-07 22:46:21 +0000

    Adding exercise for printing alphabet with some changes

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Function for writing all letters except some
+ *
+ * Return: - 0
+ */
+int main(void)
+{
+	char a;
+
+	a = 'a';
+
+	while (a < 'z')
+	{
+		if (a == 'e' || a == 'q')
+			a++;
+		putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
