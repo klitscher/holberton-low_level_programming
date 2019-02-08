a072ab9f49d52ef70a37a6cede6424856fc4a3f8
klitscher <672@holbertonschool.com>
2019-02-08 01:20:09 +0000

    Adding file for lots of singles numbers together

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Function to print two digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = '0';
+	b = '0';
+
+	while (a <= '9')
+	{
+		while (b <= '9')
+		{
+			putchar(a);
+			putchar(b);
+			if ((a & b) != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		b = '0';
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
