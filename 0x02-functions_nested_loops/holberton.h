a9e9bc12fcdf69526b6ecac729e187abbb1b94de
klitscher <672@holbertonschool.com>
2019-02-11 21:39:31 +0000

    Adding exercise for printing the alphabet a bunch. Also updated header file with same function

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -14,4 +14,46 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
+/**
+ * print_alphabet - printing the alphabet
+ *
+ * Return: 0
+ */
+int print_alphabet(void)
+{
+	char s[] = "abcdefghigklmnopqrstuvwxyz\n";
+	unsigned int i = 0;
+
+	while (i <= 26)
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	return (0);
+}
+
+/**
+ * print_alphabet_x10 - printing the alphabet 10 times
+ *
+ * Return: 0
+ */
+int print_alphabet_x10(void)
+{
+	char s[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int a = 0;
+
+	while (a < 10)
+	{
+		unsigned int i = 0;
+
+		while (i <= 26)
+		{
+			_putchar(s[i]);
+			i++;
+		}
+		a++;
+	}
+	return (0);
+}
+
 #endif /* _HOLBERTON_H_ */
