3d26364e4ef4ec42482630caecc69468819eb8f5
klitscher <672@holbertonschool.com>
2019-02-11 21:08:17 +0000

    Adding exercise to print the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - printing the alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char s[] = "abcdefghigklmnopqrstuvwxyz\n";
+	unsigned int i = 0;
+
+	while(i <= 26)
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	return (0);
+}
