c38bdb3b00a0019086c59bad90b662622caee9e4
klitscher <672@holbertonschool.com>
2019-02-12 20:57:27 +0000

    Adding files for times table exercise

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,17 +9,35 @@ void times_table(void)
 {
 	int n = 0;
 	int i = 0;
-	int num = 0;
+	int num;
+	/*int num2;*/
 
-	while (n <= 10)
+	while (n <= 9)
 	{
-		while (i <= 10)
+		while (i <= 9)
 		{
-			num = i * n;
-			_putchar(num);
-			_putchar(',');
-			_putchar(' ');
-			i ++;
+			num = n * i;
+			if (num <= 9)
+			{
+				_putchar(num + '0');
+				if (i < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0');
+				if (i < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			i++;
 		}
 		i = 0;
 		_putchar('\n');
