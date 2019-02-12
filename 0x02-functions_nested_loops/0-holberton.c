0d5a648523061e96f7775ce2a1ec1f7bb65b61ca
klitscher <672@holbertonschool.com>
2019-02-12 17:06:27 +0000

    trying without defining array length

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char h[10] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\0'};
+	char h[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\0'};
 	int i = 0;
 
 	while (i < 10)
