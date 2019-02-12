ac61f6131a2c283387bf1925a47b323dbe1d0b98
klitscher <672@holbertonschool.com>
2019-02-12 22:16:11 +0000

    Adding exercise for adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - function to add numbers
+ * @a: 1st number to add
+ * @b: 2nd number to add
+ *
+ * Return: a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
