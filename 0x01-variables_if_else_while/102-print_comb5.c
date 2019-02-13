d9c4cd07afbd0e79f2863db90e020e98022bb882
klitscher <672@holbertonschool.com>
2019-02-13 20:33:00 +0000

    Updating program to be simpler and use less variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - Function to print out all double digits, but some
+ * main - prints out all the characters
  *
  * Return: 0
  */
 int main(void)
 {
-	int a = '0';
-	int b = '0';
-	int c = '0';
-	int d = '0';
+	int a, b;
 
-	while (a <= '9')
+	for (a = 0; a <= 99; a++)
 	{
-		while (b <= '9')
+		for (b = 0; b <= 99; b++)
 		{
-			while (c <= '9')
+			if (a < b)
 			{
-				while (d <= '9')
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 98 || b != 99)
 				{
-					if((a != '9' || b != '9') && (c != '9' || d != '9') && (b != d))
-					{
-						putchar(a);
-						putchar(b);
-						putchar(' ');
-						putchar(c);
-						putchar(d);
-						if (a != '9' || b != '9' || c != '9' || d != '9')
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-					d++;
+					putchar(',');
+					putchar(' ');
 				}
-				d = '0';
-				c++;
 			}
-			c = '0';
-			b++;
 		}
-		b = '0';
-		a++;
 	}
 	putchar('\n');
 	return (0);
