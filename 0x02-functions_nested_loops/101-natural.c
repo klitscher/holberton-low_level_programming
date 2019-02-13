4b39e0af6b8d2070fd6c1daf8020fdef603127d7
klitscher <672@holbertonschool.com>
2019-02-13 02:04:54 +0000

    Adding exercise for adding numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print out natural numbers
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	int t;
+	int f;
+	int tsum = 0;
+	int fsum = 0;
+	int total;
+
+	for (t = 0; t < 1024; t += 3)
+		tsum = t + tsum;
+	for (f = 0; f < 1024; f += 5)
+		fsum = f + fsum;
+	total = fsum + tsum;
+	printf("%i\n", total);
+	return (0);
+}
