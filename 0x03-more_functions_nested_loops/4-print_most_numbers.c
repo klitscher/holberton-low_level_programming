bc793f2b9af70b6970567d811cfed61f42ffbc65
klitscher <672@holbertonschool.com>
2019-02-14 15:58:04 +0000

    Adding files for exercise about printing most numbers but a few

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print numbers except for certain ones
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (n != '2' && n != '4')
+			_putchar(n);
+	}
+	_putchar('\n');
+}
