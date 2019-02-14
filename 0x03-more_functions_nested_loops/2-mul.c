35f15ccca77c1dbd132004d70133b3d5f5c885c9
klitscher <672@holbertonschool.com>
2019-02-14 15:41:36 +0000

    Adding files for multiplication exercise

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - Multiplies two digits
+ * @a: 1st number to be multiplied
+ * @b: 2nd number to be multiplied
+ *
+ * Return: a multiplied by b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
