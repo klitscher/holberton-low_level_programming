3aad39443eb4c1dc54cf29682755b6e182199b0a
klitscher <672@holbertonschool.com>
2019-02-14 20:12:05 +0000

    Adding exercise that makes me better than 99.5 percent of programmers

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - fizz buzzzzzzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 5) == 0 && (i % 3) == 0)
+			printf("FizzBuzz ");
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+			if (i == 100)
+				printf("\n");
+		}
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
