01262577c7f6bbae720fd233afb0ec54243cceb4
klitscher <672@holbertonschool.com>
2019-02-14 20:36:53 +0000

    Updating file so newline is outside of loop

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,12 @@ int main(void)
 		if ((i % 5) == 0 && (i % 3) == 0)
 			printf("FizzBuzz ");
 		else if ((i % 5) == 0)
-		{
 			printf("Buzz ");
-			if (i == 100)
-				printf("\n");
-		}
 		else if ((i % 3) == 0)
 			printf("Fizz ");
 		else
 			printf("%d ", i);
 	}
+	printf("\n");
 	return (0);
 }
