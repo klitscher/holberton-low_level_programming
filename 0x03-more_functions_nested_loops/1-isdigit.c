f1e8219ed75b7c12c310b36b39db491b47a68647
klitscher <672@holbertonschool.com>
2019-02-14 15:36:00 +0000

    Adding files for exercise about testing digits

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - Checks to see if input is a digit
+ * @c: variable to be checked
+ *
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
