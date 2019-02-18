64f450e2106bf50a08036a4b1121b7d2733e0fbf
klitscher <672@holbertonschool.com>
2019-02-18 20:50:47 +0000

    Adding files for exercise about finding the length of a string

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string whos length is to be found
+ *
+ * Return: the length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
