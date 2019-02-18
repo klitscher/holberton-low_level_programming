d94c579f1429f009840e2e15bcde054b221b71f6
klitscher <672@holbertonschool.com>
2019-02-18 19:56:28 +0000

    Adding files for updating an int parameter

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - function to change a value using pointers
+ * @*n: pointer variable
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
