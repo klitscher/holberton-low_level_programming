9ff2c131b71aecd6c208b2e03f3074ae08a3fcda
klitscher <672@holbertonschool.com>
2019-02-22 03:29:58 +0000

    Adding files for turning 1st letter of every word uppercase

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalize all the words of a string
+ * @str: string to capitalize
+ *
+ * Return: capitalized string
+ */
+char *cap_string(char *str)
+{
+	char a[] = ". \t\n,;!?\"(){}";
+	int i, j;
+
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0  && str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (str[i] == a[j])
+			{
+				i++;
+				if (str[i] >= 97 && str[i] <= 122)
+				{
+					str[i] = str[i] - 32;
+				}
+			}
+		}
+	}
+	return(str);
+}
