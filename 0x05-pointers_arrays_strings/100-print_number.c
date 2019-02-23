3d19600e2e8789942c0d7831d3ec3016a71a681d
klitscher <672@holbertonschool.com>
2019-02-23 01:40:50 +0000

    Adding files for printing any integer with putchar

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_number - print any int
+ * @n: number to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+
+}
