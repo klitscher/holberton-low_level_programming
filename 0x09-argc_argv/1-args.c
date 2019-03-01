385a56811748d9265abc412161175b7938c485c6
klitscher <672@holbertonschool.com>
2019-03-01 20:44:56 +0000

    Adding file for printing out the number of arguments

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to the program
+ * @argc: Number of arguments passed
+ * @argv: Arguments passed
+ *
+ * Return: 0;
+ */
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
