7e9e7d8260eb27c86475641302a18acf569af52c
klitscher <672@holbertonschool.com>
2019-03-01 20:38:43 +0000

    Adding header and exercise file for printing program name

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Program that prints its name
+ * @argc: number of arguments passed to the program
+ * @argv: a one-dimensional arrray consisting of the arguments from argc
+ *
+ * Return: 0
+ */
+int main(__attribute__ ((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
