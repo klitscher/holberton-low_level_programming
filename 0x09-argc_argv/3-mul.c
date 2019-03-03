627cd15a0b5a63f73c37d0ac1a8ada6104208756
klitscher <672@holbertonschool.com>
2019-03-03 01:30:33 -0500

    Updating file to take into account all cases

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -11,6 +11,8 @@
  */
 int main(int argc, char *argv[])
 {
+	int i;
+	int j;
 	int num1;
 	int num2;
 	int mult;
@@ -19,6 +21,21 @@ int main(int argc, char *argv[])
 	{
 		printf("ERROR\n");
 		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
+				continue;
+			else if (argv[i][j] == '-' && j == 0)
+				continue;
+			else
+			{
+				printf("ERROR\n");
+				return (1);
+			}
+		}
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
