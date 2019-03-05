0a3bee6b8c4beefbb46a089878b6e70aa7ccb3d6
klitscher <672@holbertonschool.com>
2019-03-05 19:19:49 +0000

    Updating file to check if array is equal to NULL

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -16,6 +16,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
 	while (i < size)
 	{
 		array[i] = c;
