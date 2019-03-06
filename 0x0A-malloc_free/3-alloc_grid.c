add1f825d38ca1115f54437252f93e885846414e
klitscher <672@holbertonschool.com>
2019-03-06 03:18:22 +0000

    Updating file to take out typecasting

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -13,12 +13,12 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	p = (int **) malloc(height * sizeof(int *));
+	p =  malloc(height * sizeof(*p));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		p[i] = (int *) malloc(width * sizeof(int));
+		p[i] = malloc(width * sizeof(int));
 		if (p[i] == NULL)
 			return (NULL);
 	}
