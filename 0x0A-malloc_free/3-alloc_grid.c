df25f868cfd94eb6dc12424d05424d1662015757
klitscher <672@holbertonschool.com>
2019-03-06 02:55:50 +0000

    Updating file to take into account test cases

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -11,9 +11,17 @@ int **alloc_grid(int width, int height)
 {
 	int **p, i, j;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
 	p = (int **) malloc(height * sizeof(int *));
+	if (p == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
+	{
 		p[i] = (int *) malloc(width * sizeof(int));
+		if (p[i] == NULL)
+			return (NULL);
+	}
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 		{
