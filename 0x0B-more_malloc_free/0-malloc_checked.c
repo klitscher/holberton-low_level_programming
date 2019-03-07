e50cc2bb302db7ae460013bccece79b0da7e3e51
klitscher <672@holbertonschool.com>
2019-03-07 18:07:45 +0000

    Adding files for malloc exercise and also setting up new folder

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: number to allocate memory for
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr =  malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return(ptr);
+}
