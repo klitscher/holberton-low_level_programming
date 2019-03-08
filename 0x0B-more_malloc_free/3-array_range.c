c7ca49a540da9bdca2a2a2733e36c555d4adc307
klitscher <672@holbertonschool.com>
2019-03-08 02:55:32 +0000

    Updating file to take into account test case

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -13,6 +13,8 @@ int *array_range(int min, int max)
 	int *ptr;
 	int i = 0;
 
+	if (min > max)
+		return (NULL);
 	ptr = malloc(((max + 1) - min) * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
