1393d8855c328576ae28fa725c190700a17d0134
klitscher <672@holbertonschool.com>
2019-03-08 02:52:14 +0000

    Adding files for exercise 3

---
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * array_range - write an array of integers
+ * @min: lowest value
+ * @max: highest value
+ *
+ * Return: pointer to the new array
+ */
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i = 0;
+
+	ptr = malloc(((max + 1) - min) * sizeof(int));
+	if (ptr == NULL)
+		return (NULL);
+	while (min <= max)
+	{
+		ptr[i] = min;
+		i++;
+		min++;
+	}
+	return (ptr);
+}
