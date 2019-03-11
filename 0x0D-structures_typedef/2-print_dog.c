1dc72bf5ffea2b5724dd70eef57259de71003c9a
klitscher <672@holbertonschool.com>
2019-03-11 23:18:42 +0000

    Updating to be correct format for printf

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -14,15 +14,15 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("(nil)\n");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age == 0)
-		printf("0.000000");
+		printf("Age: 0.000000");
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("(nil)\n");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
