ad98b0a8bcaa62bede639d2f0196f245f6cc9413
klitscher <672@holbertonschool.com>
2019-03-11 22:59:37 +0000

    Updating file to say nil for age

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -17,8 +17,8 @@ void print_dog(struct dog *d)
 		printf("(nil)");
 	else
 		printf("Name: %s\n", d->name);
-	if (d->age == 0)
-		printf("0.000000");
+	if (d->age == 0.000000)
+		printf("(nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
