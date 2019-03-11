0eaf383926ef5ca569e4c12151baebe1ad9c1a10
klitscher <672@holbertonschool.com>
2019-03-11 23:19:58 +0000

    Adding newline

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age == 0)
-		printf("Age: 0.000000");
+		printf("Age: 0.000000\n");
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
