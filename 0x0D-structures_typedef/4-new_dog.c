f6c02b04e74892d9b0e4e943776bd887fd06b804
klitscher <672@holbertonschool.com>
2019-03-12 02:15:57 +0000

    Updating file by not freeing anything

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -10,6 +10,7 @@
 int _strlen(char *s)
 {
 	int i;
+
 	if (s == NULL)
 		return (0);
 	for (i = 0; *s != '\0'; s++)
@@ -34,7 +35,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int i = 0;
 	int len_name = _strlen(name);
 	int len_owner = _strlen(owner);
-
 	/* Allocating memory to store struct,  name and owner strings */
 	ptr_new_dog = malloc(sizeof(dog_t));
 	if (ptr_new_dog == NULL)
@@ -42,28 +42,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr_name = malloc(sizeof(name) * len_name + 1);
 	if (ptr_name == NULL)
 	{
-		free(ptr_name);
 		return (NULL);
 	}
 	ptr_owner = malloc(sizeof(owner) * len_owner + 1);
 	if (ptr_owner == NULL)
 	{
-		free (ptr_owner);
 		return (NULL);
 	}
 	/* Copying the old strings to new mem location */
-	while (i < len_name)
-	{
+	for (i = 0; i < len_name; i++)
 		ptr_name[i] = name[i];
-		i++;
-	}
 	ptr_name[i] = '\0';
-	i = 0;
-	while (i < len_owner)
-	{
+	for (i = 0; i < len_owner; i++)
 		ptr_owner[i] = owner[i];
-		i++;
-	}
 	ptr_owner[i] = '\0';
 	/* setting the values of new struct */
 	ptr_new_dog->name = ptr_name;
