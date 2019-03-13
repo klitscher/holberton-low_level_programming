7cfe2a271bae5607e48e18ccc8b28ce0a7e6c6c7
klitscher <672@holbertonschool.com>
2019-03-13 17:50:47 +0000

    Adding header, putchar, and files for the first exercise

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Function to print out a name
+ * @name: name to print out
+ * @f: pointer to a function
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+	f(name);
+}
