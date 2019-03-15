3dd3408fbfe62cd990712a301ea635a1cbf96130
klitscher <672@holbertonschool.com>
2019-03-15 16:06:42 +0000

    Adding files for printing a string

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: separates the strings
+ * @n: number of arguments
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list va_string;
+	char *string;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+
+	va_start(va_string, n);
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(va_string, char *);
+		if (string == NULL)
+			printf("(nil)");
+		else
+			printf("%s", string);
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
