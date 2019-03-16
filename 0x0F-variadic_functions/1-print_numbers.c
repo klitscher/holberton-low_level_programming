171aa9044c0e1cd09ccb79bebe148eece8d07e80
klitscher <672@holbertonschool.com>
2019-03-16 16:41:13 +0000

    Moving va_end to before the print

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -27,6 +27,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i !=  n - 1 && *separator != NULL)
 			printf("%s", separator);
 	}
-	printf("\n");
 	va_end(va_numbers);
+	printf("\n");
 }
