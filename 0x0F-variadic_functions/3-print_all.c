e1a33850bab38e0edb988db10fa1030f4b46f6aa
klitscher <672@holbertonschool.com>
2019-03-16 20:27:16 +0000

    Updated file to separate into two functions

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -4,6 +4,21 @@
 #include "variadic_functions.h"
 
 /**
+ * _helper - prints the divider
+ * @pf: pointer to a char
+ *
+ * Return: void
+ */
+void _helper(const char *pf)
+{
+	while (*pf == 'c' || *pf == 'i' || *pf == 'f' || *pf == 's')
+	{
+		if (*(pf + 1) != '\0')
+			printf(", ");
+		break;
+	}
+}
+/**
  * print_all - prints anything
  * @format: list of types of arguments passed to function
  *
@@ -12,11 +27,10 @@
 void print_all(const char * const format, ...)
 {
 	va_list va_print;
-	const char *pf;
+	const char *pf = format;
 	char *temp;
 
 	va_start(va_print, format);
-	pf = format;
 	while (format != NULL && *pf != '\0')
 	{
 		switch (*pf)
@@ -43,12 +57,7 @@ void print_all(const char * const format, ...)
 			pf++;
 			continue;
 		}
-		while (*pf == 'c' || *pf == 'i' || *pf == 'f' || *pf == 's')
-		{
-			if (*(pf + 1) != '\0')
-				printf(", ");
-			break;
-		}
+		_helper(pf);
 		pf++;
 	}
 	va_end(va_print);
