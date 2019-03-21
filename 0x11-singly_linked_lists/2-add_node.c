e00e05ceaa6a1b680569a33ec315acc15b7068a3
klitscher <672@holbertonschool.com>
2019-03-21 23:19:07 +0000

    Adding files for creating a new node in the beginning of a list

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * _strlen - find the length of a string
+ * @str: the string to find the length of
+ *
+ * Return: the length of the string: i
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * add_node - adds a new node to a list
+ * @head: pointer to a pointer to a list
+ * @str: string to place in the node
+ *
+ * Return: address of the new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->str = strdup(str);
+	temp->len = strlen(str);
+	temp->next = *head;
+	*head = temp;
+	return (temp);
+}
