0e6fa9d56875e02afeb6c6b967a80cacf6bd5710
klitscher <672@holbertonschool.com>
2019-03-22 20:18:41 +0000

    Adding files for executing a function before main runs

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * premain - Thing
+ *
+ * Return: void
+*/
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
