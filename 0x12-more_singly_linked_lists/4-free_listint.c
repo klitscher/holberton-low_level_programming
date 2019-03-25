a920ea80fbf18b42f530e043d77205775ac09c7c
klitscher <672@holbertonschool.com>
2019-03-25 18:43:38 +0000

    Adding files for freeing all the things

---
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint
+ * @head: list to free
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	ptr = head;
+	while (ptr != NULL)
+	{
+		head = ptr;
+		ptr = ptr->next;
+		free (head);
+	}
+}
