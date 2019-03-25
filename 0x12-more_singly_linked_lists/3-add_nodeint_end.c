5e1329da5a5a0c17dececf9642cb4d98066d2d29
klitscher <672@holbertonschool.com>
2019-03-25 17:52:21 +0000

    Adding files for exercise 3

---
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add a new node at the end of a list
+ * @n: number
+ * @head: position of the first node
+ *
+ * Return: address of new element or NULL
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node, *end;
+
+	end = *head;
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+
+	while (end->next != NULL)
+	{
+		end = end->next;
+	}
+	end->next = new_node;
+	return (end);
+}
