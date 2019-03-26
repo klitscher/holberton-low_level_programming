7d578640055dbc1a387713d5aac45f5dbef690f3
klitscher <672@holbertonschool.com>
2019-03-26 14:52:15 +0000

    Updated file to make betty databale

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -3,14 +3,14 @@
 #include "lists.h"
 
 /**
- * print_listin - Prints the elements of a list
+ * print_listint - Prints the elements of a list
  * @h: list to print
  *
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
 	if (h == NULL)
 		return (0);
