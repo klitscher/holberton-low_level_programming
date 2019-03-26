9f2e62a05d1ee5ceb6a5e805536917c33fa97c80
klitscher <672@holbertonschool.com>
2019-03-26 18:00:31 +0000

    Nuked original in fire, uploading new test

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,27 +15,40 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *before, *after, *new_node;
 	unsigned int i = 1;
 
+	if (head == NULL)
+		return (NULL);
+	if (*head == NULL && idx != 0)
+		return (NULL);
+	if (idx == 0)
+	{
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	before = *head;
 	after = *head;
-	/*Iterating through list till it gets to correct index*/
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
 	while (i < idx)
 	{
 		if (before->next == NULL)
-			return (NULL);
+		{
+			before->next = new_node;
+			return (new_node);
+		}
 		before = before->next;
 		after = after->next;
 		i++;
 	}
-	/*moving one past the correct index*/
 	after = after->next;
-	/* Creating new node */
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-
 	before->next = new_node;
-	new_node->n = n;
 	new_node->next = after;
-
 	return (new_node);
 }
