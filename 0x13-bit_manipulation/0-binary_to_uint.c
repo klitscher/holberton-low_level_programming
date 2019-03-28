a45614d1eefbc6b163045e1a8b265631e663bd35
klitscher <672@holbertonschool.com>
2019-03-28 17:28:05 +0000

    Updating file to check for 0 being the only character

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -16,6 +16,8 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
+	if (b[0] == '0' && b[1] == '\0')
+		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
