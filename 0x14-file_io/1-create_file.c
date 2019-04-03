1335379953f4b27b3a809bbcf56e38b62b59eaf3
klitscher <672@holbertonschool.com>
2019-04-03 01:24:28 +0000

    Adding file for creating a new file

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * create_file - function to create a file
+ * @filename: name of the new file
+ * @text_content: contents to put in the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i;
+
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+	}
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+		close(fd);
+		return (1);
+	}
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, i);
+	return (1);
+}
