814d336d79b11914e8d9018ab16cc1597698cd42
klitscher <672@holbertonschool.com>
2019-04-03 02:18:07 +0000

    Updated to write only

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_RDONLY);
+		fd = open(filename, O_WRONLY);
 		if (fd == -1)
 			return (-1);
 		close(fd);
