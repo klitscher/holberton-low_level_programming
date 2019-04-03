9b00117c5b8cdefb0bd8c7f9598abbc2ec07dedf
klitscher <672@holbertonschool.com>
2019-04-03 20:18:16 +0000

    Updating file to work

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -14,36 +14,58 @@
  */
 int main(int argc, const char *argv[])
 {
-	int from, to, i;
+	int from, to;
+	ssize_t b_r, b_w, close_it;
 	char buff[1024] = {'\0'};
 
 	if (argc != 3)
 	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
 		exit(98);
 	}
 
 	to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (to == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
 		exit(99);
 	}
-	read(from, buff, 157);
+	do {
+		b_r = read(from, buff, 1024);
+		if (b_r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		if (b_r == 0)
+			break;
+		b_w = write(to, buff, b_r);
+		if (b_w < 1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+			exit(99);
+		}
+	} while (1);
 
-	for (i = 0; buff[i] != '\0'; i++)
+	close_it = close(from);
+	if (close_it == -1)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+		exit(100);
+	}
+	close_it = close(to);
+	if (close_it == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
+		exit(100);
 	}
 
-	write(to, buff, i);
-
-	close(from);
-	close(to);
 	return (0);
 }
