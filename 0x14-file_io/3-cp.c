cdf6131b6d0fd1c417ab31aeb378a2a1720dabcd
klitscher <672@holbertonschool.com>
2019-04-04 02:08:28 +0000

    Broke into two functions to be betty compliant

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -6,16 +6,45 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
+ * read_write - read and write from buffer
+ * @fd: First file descriptor
+ * @fd2: Second file descriptor
+ * @buff: Buffer
+ * @argv1: First command line argument
+ * @argv2: Second command line argument
+ */
+void read_write(int fd, int fd2, char *buff, char *argv1, char *argv2)
+{
+	ssize_t b_r, b_w;
+
+	do {
+		b_r = read(fd, buff, 1024);
+		if (b_r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv1);
+			exit(98);
+		}
+		if (b_r == 0)
+			return;
+		b_w = write(fd2, buff, b_r);
+		if (b_w < 1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv2);
+			exit(99);
+		}
+	} while (1);
+}
+/**
  * main - reads a textfile and prints it to POSIX standard output
  * @argc: Number of arguments
  * @argv: Array of strings
  *
  * Return: Actual number of letters it could read and print
  */
-int main(int argc, const char *argv[])
+int main(int argc, char *argv[])
 {
-	int from, to;
-	ssize_t b_r, b_w, close_it;
+	int from, to, close_it;
+
 	char buff[1024] = {'\0'};
 
 	if (argc != 3)
@@ -37,22 +66,7 @@ int main(int argc, const char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	do {
-		b_r = read(from, buff, 1024);
-		if (b_r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		if (b_r == 0)
-			break;
-		b_w = write(to, buff, b_r);
-		if (b_w < 1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	} while (1);
+	read_write(from, to, buff, argv[1], argv[2]);
 
 	close_it = close(from);
 	if (close_it == -1)
