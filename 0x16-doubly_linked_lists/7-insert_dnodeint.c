261e02d86b7b13546eb1267a1e97bc0834bcc572
klitscher <672@holbertonschool.com>
2019-05-04 14:01:06 -0400

    Update for if *head == NULL

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *current, *past;
 	int count = idx;
 
-	if (h == NULL)
+	if (h == NULL || *h == NULL)
 		return (NULL);
 	current = *h;
 	past = *h;
