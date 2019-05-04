20af21078b5dd19de16267ef112a2f060934cb91
klitscher <672@holbertonschool.com>
2019-05-04 14:40:40 -0400

    Update for style guide

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *current, *past;
 	int count = idx;
 	unsigned int check = 0;
-	
+
 	if (h == NULL)
 		return (NULL);
 	current = *h;
