07a63b514ababaea4fccbbddfbbcab5f5bb98fc6
klitscher <672@holbertonschool.com>
2019-05-06 23:11:12 +0000

    Update for betty

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/klitscher/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
-export LD_PRELOAD=$PWD/win.so
+wget https://tinyurl.com/yyhcav73
+LD_PRELOAD=PWD/win.so ./gm
