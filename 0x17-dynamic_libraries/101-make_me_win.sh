769cd55d8b1edef81a8ef688838809b45ad45178
klitscher <672@holbertonschool.com>
2019-05-06 23:15:21 +0000

    Update to add $

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget https://github.com/klitscher/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
-export LD_PRELOAD=PWD/win.so
+export LD_PRELOAD=$PWD/win.so
