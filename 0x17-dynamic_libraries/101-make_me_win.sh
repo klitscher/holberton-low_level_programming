d9b6e44ad2597fd00ed3c779ed52b82c8cf8c6d9
klitscher <672@holbertonschool.com>
2019-05-06 23:17:06 +0000

    Trying with a smaller link

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/klitscher/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+wget https://tinyurl.com/yyhcav73
 export LD_PRELOAD=$PWD/win.so
