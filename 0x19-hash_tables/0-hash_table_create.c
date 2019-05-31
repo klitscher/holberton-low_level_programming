2bc5c33f2b208705cf8be1628ba1748dc5540314
klitscher <672@holbertonschool.com>
2019-05-31 16:21:35 -0400

    Add files for exercise 1

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: size of the array
+ *
+ * Return: Pointer to the new hash table or NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = calloc(1, sizeof(*ht));
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
+}
