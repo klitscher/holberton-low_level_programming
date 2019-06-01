c3349e29805a82cc978ce958a195f91cdc2d08c3
klitscher <672@holbertonschool.com>
2019-05-31 20:24:39 -0400

    Updated for my clarity

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 
-	ht = calloc(1, sizeof(*ht));
+	ht = calloc(1, sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 	ht->size = size;
