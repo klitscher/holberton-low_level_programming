b46cfacd46eaf5a96f6545218f1553ef53424b98
klitscher <672@holbertonschool.com>
2019-06-02 01:49:46 +0000

    Add exercise 6, still need to troubleshoot valgrind error

---
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete a hash table
+ * @ht: Hash table
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	int i, size;
+	hash_node_t *head, *next;
+
+	if (ht != NULL)
+	{
+		size = ht->size;
+		for (i = 0; i < size; i++)
+		{
+			if (ht->array[i] != NULL)
+			{
+				head = ht->array[i];
+				while (head != NULL)
+				{
+					next = head->next;
+					free(head->value);
+					free(head);
+					head = next;
+				}
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
