fc93cc88bd7411b75303f4fb16b2cd919f3269c8
klitscher <672@holbertonschool.com>
2019-06-03 16:04:48 -0400

    Trying strdup

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	char *vcpy = NULL, *kcpy = NULL;
-	hash_node_t *head;
+	hash_node_t *head = NULL;
 	unsigned long int idx = 0;
 
 	if (ht == NULL || ht->size < 1 || ht->array == NULL ||
@@ -28,7 +28,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(ht);
 		return (0);
 	}
-	vcpy = malloc(sizeof(char) * strlen(value) + 1);
+	/*vcpy = malloc(sizeof(char) * strlen(value) + 1);*/
+	vcpy = strdup(value);
 	if (vcpy == NULL)
 	{
 		free(head);
@@ -36,7 +37,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(ht);
 		return (0);
 	}
-	kcpy = malloc(sizeof(char) * strlen(key) + 1);
+	/*kcpy = malloc(sizeof(char) * strlen(key) + 1);*/
+	kcpy = strdup(key);
 	if (kcpy == NULL)
 	{
 		free(vcpy);
@@ -45,8 +47,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(ht);
 		return (0);
 	}
-	strcpy(vcpy, value);
-	strcpy(kcpy, key);
 	head->value = vcpy;
 	head->key = kcpy;
 	head->next = NULL;
@@ -66,7 +66,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 int ht_add_node(hash_table_t *ht, hash_node_t *head,
 		 int idx, const char *key)
 {
-	hash_node_t  *fast;
+	hash_node_t  *current;
 
 	if (ht->array[idx] == NULL)
 	{
@@ -74,18 +74,18 @@ int ht_add_node(hash_table_t *ht, hash_node_t *head,
 	}
 	else
 	{
-		fast = ht->array[idx];
-		while (fast != NULL)
+		current = ht->array[idx];
+		while (current != NULL)
 		{
-			if (strcmp(fast->key, key) == 0)
+			if (strcmp(current->key, key) == 0)
 			{
-				free(fast->value);
-				fast->value = head->value;
+				free(current->value);
+				current->value = head->value;
 				free(head->key);
 				free(head);
 				return (0);
 			}
-			fast = fast->next;
+			current = current->next;
 		}
 		head->next = ht->array[idx];
 		ht->array[idx] = head;
