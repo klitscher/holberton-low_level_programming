2d9f9fdb111be7c7d813fc819e6beb14cb6c7df3
klitscher <672@holbertonschool.com>
2019-07-23 00:17:25 +0000

    Updating files for exercises 10-13

--- 0x1D-binary_trees/10-binary_tree_depth.c
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -1,11 +1,23 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth -
+ * binary_tree_depth - find the depth of a tree
+ * @tree: tree to find depth
  *
- * Return: 
+ * Return: Depth of tree
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t count = 0;
 
+	if (tree == NULL)
+		return (0);
+
+
+	while (tree->parent)
+	{
+		tree = tree->parent;
+		count++;
+	}
+	return (count);
 }
