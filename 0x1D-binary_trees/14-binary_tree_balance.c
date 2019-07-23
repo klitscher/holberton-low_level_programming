13098fa24e5785ee63b9fe79557bd329f18af7ca
klitscher <672@holbertonschool.com>
2019-07-23 01:48:06 +0000

    Updating file for exercise 14

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,11 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance -
+ * binary_tree_balance - measures balance factor of a binary tree
+ * @tree: tree to get the balance of
  *
- * Return: 
+ * Return: balance factor
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int balance;
 
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	if (tree->left == NULL)
+		return (-1);
+	if (tree->right == NULL)
+		return (1);
+
+	balance = binary_tree_balance(tree->left->left) - binary_tree_balance(tree->right->right) + 1;
+	return (balance);
 }
