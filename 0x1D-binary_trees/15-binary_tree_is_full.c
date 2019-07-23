b1718357b8acb73908e1da886edb10abd59153a1
klitscher <672@holbertonschool.com>
2019-07-23 03:21:06 +0000

    Updating file for exercise 15

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -1,11 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full -
+ * binary_tree_is_full - check if tree is full
+ * @tree: pointer to tree
  *
- * Return: 
+ * Return: 0 if false, 1 if true
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+	}
+	return (0);
 }
