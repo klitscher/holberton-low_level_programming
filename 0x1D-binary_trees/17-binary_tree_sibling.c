b79ca3fe28e689c2b87407b96d7def21c05e158d
klitscher <672@holbertonschool.com>
2019-07-23 17:06:03 +0000

    Updating files for exercises 17 and 18

--- 0x1D-binary_trees/17-binary_tree_sibling.c
+++ 0x1D-binary_trees/17-binary_tree_sibling.c
@@ -1,11 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling -
- *
- * Return: 
- */
+ * binary_tree_sibling - finding the sibling of node
+ * @node: pointer to node to find sibling
+ * Return: Sibling node
+ **/
+
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->right == node)
+		return (node->parent->left);
+	else if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (NULL);
 
 }
--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -1,11 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle -
+ * binary_tree_uncle - Finding the uncle of a given node
+ * @node: pointer of node to find uncle of
  *
- * Return: 
+ * Return: return pointer to the uncle node
  */
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
 
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+	else if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	else
+		return (NULL);
 }
